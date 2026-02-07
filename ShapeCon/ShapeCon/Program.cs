79da0ab6adc9679152c6d5afe8c65d1c42d28e8a
LeandroOcarol <149980007+LeandroOcarol@users.noreply.github.com>
2026-02-07 16:14:15 +0800

    Add files via upload

---
+++ ShapeCon/ShapeCon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCon
+{
+    internal class Program
+    { 
+        static void Main(string[] args) { 
+
+            Shape sh = new Square();
+            
+            Console.Write("Enter Name: ");
+            sh.Name = Console.ReadLine();
+            Console.Write("Enter Color: ");
+            sh.Color = Console.ReadLine();
+            Console.Write("Enter No. of Sides: ");
+            sh.NoSide = int.Parse(Console.ReadLine());
+            Console.WriteLine(sh);
+            Console.Write("Enter side Length: ");
+            ((Square)sh).SideLength = double.Parse(Console.ReadLine());
+            Console.WriteLine(sh);
+
+            sh = new Rectangle();
+            Console.Write("Enter Name: ");
+            sh.Name = Console.ReadLine();
+            Console.Write("Enter Color: ");
+            sh.Color = Console.ReadLine();
+            Console.Write("Enter No. of Sides: ");
+            sh.NoSide = int.Parse(Console.ReadLine());
+            Console.Write("Enter  Length: ");
+            ((Rectangle)sh).Length = double.Parse(Console.ReadLine());
+            Console.Write("Enter  Width: ");
+            ((Rectangle)sh).Width = double.Parse(Console.ReadLine());
+            Console.WriteLine(sh);
+
+            sh = new Circle();
+            Console.Write("Enter Name: ");
+            sh.Name = Console.ReadLine();
+            Console.Write("Enter Color: ");
+            sh.Color = Console.ReadLine();
+            Console.Write("Enter No. of Sides: ");
+            sh.NoSide = int.Parse(Console.ReadLine());
+            Console.Write("Enter  Radius: ");
+            ((Circle)sh).Radius = double.Parse(Console.ReadLine());
+            
+        }
+    }
+}
